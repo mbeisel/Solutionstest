1c3f1d5aa078470ca5283b19c1bbacb4f186030b
martin <martin.beisel@web.de>
2017-03-31 01:43:34 +0200

    test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
 import { Component } from '@angular/core';
 
+export class Solution {
+  id: number;
+  name: string;
+  pattern: string;
+  json: string;
+  yaml: string;
+  relatedSolutions: string;
+}
+
+const SOLUTIONS: Solution[] = [
+  { id: 1,name: 'Watchdog', pattern: 'Watchdog', json: 'WATCHDOG LONGJSON',yaml: 'WATCHDOG LONGYAML',relatedSolutions: 'Autoscaling'},
+  { id: 2,name: 'Autoscaling', pattern: 'Elastic Load Balancing', json: 'AUTOSCALING LONGJSON',yaml: 'AUTOSCALING LONGYAML',relatedSolutions: 'Watchdog'}
+];
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'Solutionrepository';
+  solution = SOLUTIONS[0];
+  solutions = SOLUTIONS;
+  selectedSolution = SOLUTIONS[0];
+  onSelect(solution: string): void{
+  	for (var i = 0; i < this.solutions.length; i++) {
+  		if(this.solutions[i].name == solution){
+  			this.selectedSolution = this.solutions[i];
+			}			
+  	}
+  }
 }
---
+++ src/app/solution.ts
@@ -0,0 +1,8 @@
+export class Solution {
+  id: number;
+  name: string;
+  pattern: string;
+  json: string;
+  yaml: string;
+  relatedSolutions: string;
+}
